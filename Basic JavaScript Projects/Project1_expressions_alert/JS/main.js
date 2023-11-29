7bac39463c7647a1b7afc1e93b8385267226dabe
ayshadairi <ayshadairi@gmail.com>
2023-11-29 19:25:07 -0330

    Added folders and files in order to use Javascript. Applied some of the rules of Javascript.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,22 @@
+window.alert('Hello World!');
+document.write('Hello World!');
+
+var A='This is a string'; //here we assigned a value to a varianle
+
+document.write(A);
+
+var D= 'Second string';
+window.alert(D);
+
+document.write("Hello everyone! Today we will utilize writing \I've\ this is how you"
++ "can print characters on the screen dosn\'t it look easy?");
+
+var C= 'concatenating' + 'strings';
+document.write(C);
+
+var members='six members', org='ruby', education='teacher';
+
+document.write(org);
+
+3+3
+document.write(3+3);
