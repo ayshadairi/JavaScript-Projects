80f0c5e5d2049a7afe239304b062474feeaafa4b
ayshadairi <ayshadairi@gmail.com>
2023-12-03 12:44:25 -0330

    Created a Tic Tac Toe game

--- Basic JavaScript Projects/asunc/JS/async.js
+++ Basic JavaScript Projects/asunc/JS/async.js
@@ -1,1 +1,6 @@
 alert(document.getElementById('p1'),innerHTML);
+
+var C = document.getElementById('ID_Name');
+var ctx = C.getContext('2d');
+ctx.font='30px Arial';
+ctx.fillText("My first canvas", 10, 50);
